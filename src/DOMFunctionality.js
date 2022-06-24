09fb3e506e08eed3e7cbdbc18c37819824f2b2da
Jordan <JMCollins0@aol.com>
2022-06-24 11:58:11 -0400

    add .gitignore to node node_modules
    add DOMFunctionality.js for seperate dom functions

---
+++ src/DOMFunctionality.js
@@ -0,0 +1,12 @@
+
+/* 
+    TODO: Get a barebones UI going and create the objects using that. 
+
+*/
+
+function makeInput(){
+    const input = document.createElement("input");
+    return input;
+}
+
+export {makeInput};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import createProject from "./projectObjects";
+import { makeInput } from "./DOMFunctionality";
 
 const testProj = createProject("TestOne");
 
 testProj.createTodo("TDTit","TDDesc");
 
 console.log(testProj.list);
+
+document.body.append(makeInput())
+
+/* 
+    Go to DOM functionality and create a UI
+*/
